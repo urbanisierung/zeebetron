4dc8a7ea6291b2eedfb02aa1b84f544a6a8e6aa0
Adam <i@adamurban.de>
2020-01-12 17:52:28 +0100

    updated names

--- e2e/main.spec.ts
+++ e2e/main.spec.ts
@@ -3,7 +3,7 @@ import {SpectronClient} from 'spectron';
 
 import commonSetup from './common-setup';
 
-describe('angular-electron App', function () {
+describe('zeebetron App', function () {
   commonSetup.apply(this);
 
   let browser: any;
